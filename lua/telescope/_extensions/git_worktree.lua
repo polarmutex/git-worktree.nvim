193ead46575d84698e0b3c0ba27c6216bd18b414
Seigneurie Laurent <laurent.seigneurie@soprasteria.com>
2024-04-26 15:05:11 +0200

    [FIX]: Fix telescope hl_group warning

--- lua/telescope/_extensions/git_worktree.lua
+++ lua/telescope/_extensions/git_worktree.lua
@@ -135,8 +135,8 @@ local telescope_git_worktree = function(opts)
     local make_display = function(entry)
         return displayer {
             { entry.branch, 'TelescopeResultsIdentifier' },
-            { utils.transform_path(opts, entry.path) },
-            { entry.sha },
+            { utils.transform_path(opts, entry.path), 'TelescopeResultsComment' },
+            { entry.sha, 'TelescopeResultsNumber' },
         }
     end
 
